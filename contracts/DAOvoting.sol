34b57fa4b5c5e65a84ea1244ea98092c49400b72
admin <anandalex1993@gmail.com>
2024-06-11 12:41:28 +0530

    function desc

--- contracts/DAOvoting.sol
+++ contracts/DAOvoting.sol
@@ -3,26 +3,43 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract DAO {
-    function initializeDAO(
-        uint256 _contributionTimeEnd,
-        uint256 _voteTime,
-        uint256 _quorum
-    ) public {}
+    // @dev Initializes the DAO with specific parameters.
+    // @params _contributionTimeEnd sets the duration, in seconds from the initialization of the DAO, during which contributions are accepted.
+    // @params _voteTime this parameter sets the duration, measured in seconds from the creation of a proposal, during which voting is open.
+    // @params _quorum represents the minimum percentage of the combined total vote weightage from all contributors required for a proposal to be executed. Each vote holds a weight equivalent to the number of shares held by the individual voter.
+    function initializeDAO(uint256 _contributionTimeEnd, uint256 _voteTime, uint256 _quorum) public {}
 
-
+    // @Allows users to contribute ETH to the DAO.
+    // The amount contributed is converted into shares and added to the user's balance.
+    // The function ensures that contributions are only accepted within the set contribution time frame and that the amount is greater than zero.
     function contribution() public payable {}
 
-    function reedemShare(uint256 amount) public{}
+    // @dev Enables investors to redeem their shares in exchange for the equivalent amount in Wei (1 wei equals 1 share) from the DAO's available funds.
+    // The function checks if the investor has enough shares and if the DAO has sufficient available funds before proceeding with the redemption.
+    function reedemShare(uint256 amount) public {}
 
+    // @dev Allows investors to transfer a specified amount of their shares to another address.
+    // The function ensures that the investor has enough shares to transfer and that the DAO has enough available funds.
+    // The recipient is added to the list of investors if not already present.
     function transferShare(uint256 amount, address to) public {}
 
-    function createProposal(string calldata description,uint256 amount,address payable receipient) public {}
+    // @dev Used by the contract owner to create a new proposal for funding.
+    // The proposal includes a description, the amount requested, the recipient's address.
+    // The function checks if there are enough available funds before creating the proposal.
+    function createProposal(string calldata description, uint256 amount, address payable receipient) public {}
 
+    // @dev Allows investors to cast their votes on a specific proposal.
+    // The function ensures that an investor can only vote once per proposal and that the voting period is still open.
+    // Each vote holds a weight equivalent to the number of shares held by the individual voter.
     function voteProposal(uint256 proposalId) public {}
 
+    // @dev Can be called by the contract owner to execute a proposal after the voting period has ended.
+    // The function checks if the proposal has received enough vote weightage to meet the quorum and then transfers the requested funds to the recipient if successful.
     function executeProposal(uint256 proposalId) public {}
 
-    function proposalList() public returns (string[] ,uint[],address[]) {}
+    // @dev Returns 3 arrays, array of all the description, array of amount, and array of recipient, where each index of these represents a proposal.
+    function proposalList() public returns (string[], uint[], address[]) {}
 
+    // @dev Provides a list of all investor addresses that have contributed to the DAO. This function is useful for tracking and managing the investor base.
     function allInvestorList() public view returns (address[]) {}
 }
